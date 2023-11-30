8851b71522ca480175df0893c1ebeb703b78d4df
Stv-devl <lansadstevan@gmail.com>
2023-11-30 11:34:25 +0700

    error page react and sass

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
     <main>
-      <h1>Error 401</h1>
+      <h1 className="error">404</h1>
+      <h2 className="oups">Oups! La page que vous demandez n'existe pas.</h2>
+      <Link to={`/home`} className="backHome">
+        Retourner sur la page d'accueil
+      </Link>
     </main>
   );
 };
