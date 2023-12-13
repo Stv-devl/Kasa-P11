1d9a8bcb4f3c6745c478a626987bb5d394ebc99c
Stv-devl <lansadstevan@gmail.com>
2023-12-13 17:35:48 +0700

    hidden chevron & counter, error in id url

--- src/pages/Appartement.jsx
+++ src/pages/Appartement.jsx
@@ -1,17 +1,21 @@
 import Rent from "../components/Rent";
 import { useParams } from "react-router-dom";
 import Datas from "../services/Datas";
+import Error from "./Error";
 
 const Appartement = () => {
   const { data } = Datas();
   const { id } = useParams();
 
+  const filterData = data && data.filter((el) => el.id === id);
+  //if filterData is not null and length>0 , activate Rent composant else activate Error composant
   return (
     <main>
-      {data &&
-        data
-          .filter((el) => el.id === id)
-          .map((item) => <Rent key={item.id} data={item} />)}
+      {filterData && filterData.length > 0 ? (
+        filterData.map((item) => <Rent key={item.id} data={item} />)
+      ) : (
+        <Error />
+      )}
     </main>
   );
 };
